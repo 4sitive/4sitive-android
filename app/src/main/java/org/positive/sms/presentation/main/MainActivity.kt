ecc20f2d1064bf1db696a09753813741ac296d6c
Yonghun Kim <sabgilhun@gmail.com>
2021-05-07 02:03:04 +0900

    fix error

--- app/src/main/java/org/positive/sms/presentation/main/MainActivity.kt
+++ app/src/main/java/org/positive/sms/presentation/main/MainActivity.kt
@@ -14,6 +14,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.viewModel = viewModeld
+        binding.viewModel = viewModel
     }
 }
