3cbde200ded8bb4efaa191f087d435dea164b950
Yonghun Kim <sabgilhun@gmail.com>
2021-05-15 05:15:06 +0900

    implement to post auth code

---
+++ app/src/main/java/org/positive/sms/presentation/login/LoginViewModel.kt
@@ -0,0 +1,37 @@
+package org.positive.sms.presentation.login
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.positive.sms.data.model.GrantType
+import org.positive.sms.data.model.PostOauthAuthorizationCodeRequest
+import org.positive.sms.data.pref.AppSharedPreference
+import org.positive.sms.data.repository.AuthRepository
+import org.positive.sms.presentation.base.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val authRepository: AuthRepository,
+    private val sharedPreferences: AppSharedPreference
+) : BaseViewModel() {
+
+    fun postAuthorizationCode(code: String) {
+        val request = PostOauthAuthorizationCodeRequest(
+            clientId = "4sitive",
+            clientSecret = "secret",
+            code = code,
+            grantType = GrantType.AUTHORIZATION_CODE,
+            redirectUri = "http://127.0.0.1:8080/authorized"
+        )
+
+        authRepository.postOauthAuthorizationCode(request)
+            .compose(apiLoading())
+            .autoDispose {
+                success {
+                    sharedPreferences.authToken = it.accessToken
+                }
+                error {
+                    showErrorMessage(it.message.orEmpty())
+                }
+            }
+    }
+}
