fcd332eefa05385106fc4b711813ffbec5b3400d
Yonghun Kim <sabgilhun@gmail.com>
2021-06-06 04:03:03 +0900

    add build variants for crashlytics

--- app/src/main/java/org/positive/daymotion/PsApplication.kt
+++ app/src/main/java/org/positive/daymotion/PsApplication.kt
@@ -1,7 +1,16 @@
 package org.positive.daymotion
 
 import android.app.Application
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PsApplication : Application()
+class PsApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        FirebaseCrashlytics
+            .getInstance()
+            .setCrashlyticsCollectionEnabled(BuildConfig.BUILD_TYPE != "debug")
+    }
+}
