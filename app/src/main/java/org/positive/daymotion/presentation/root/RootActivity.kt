f5f679dcda4b96206b3d2539888a07547bda86c8
Yonghun Kim <sabgilhun@gmail.com>
2021-06-07 23:14:03 +0900

    connect taps to the root screen

--- app/src/main/java/org/positive/daymotion/presentation/root/RootActivity.kt
+++ app/src/main/java/org/positive/daymotion/presentation/root/RootActivity.kt
@@ -2,6 +2,7 @@ package org.positive.daymotion.presentation.root
 
 import android.content.Context
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import org.positive.daymotion.R
@@ -9,6 +10,10 @@ import org.positive.daymotion.databinding.ActivityRootBinding
 import org.positive.daymotion.extension.startOnTop
 import org.positive.daymotion.presentation.base.BaseActivity
 import org.positive.daymotion.presentation.base.util.viewModelOf
+import org.positive.daymotion.presentation.root.tabs.category.CategoryTabFragment
+import org.positive.daymotion.presentation.root.tabs.home.HomeTabFragment
+import org.positive.daymotion.presentation.root.tabs.my.MyTabFragment
+import org.positive.daymotion.presentation.root.tabs.setting.SettingTabFragment
 
 @AndroidEntryPoint
 class RootActivity : BaseActivity<ActivityRootBinding>(R.layout.activity_root) {
@@ -21,8 +26,20 @@ class RootActivity : BaseActivity<ActivityRootBinding>(R.layout.activity_root) {
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            // TODO(je): create fragment and setting listener
+            val tab = when (item.itemId) {
+                R.id.menu_home -> HomeTabFragment()
+                R.id.menu_category -> CategoryTabFragment()
+                R.id.menu_my -> MyTabFragment()
+                R.id.menu_setting -> SettingTabFragment()
+                else -> throw IllegalStateException("invalid bottom nav menu id")
+            }
+            supportFragmentManager.commit {
+                replace(R.id.container, tab)
+            }
             true
+        }
+        supportFragmentManager.commit {
+            replace(R.id.container, HomeTabFragment())
         }
     }
 
