a2fa6b632871d6df28a904fc4d40ffa0120a0fcc
Yonghun Kim <sabgilhun@gmail.com>
2021-06-07 22:10:36 +0900

    Improve structure of subscriber classes

--- app/src/main/java/org/positive/daymotion/presentation/base/BaseViewModel.kt
+++ app/src/main/java/org/positive/daymotion/presentation/base/BaseViewModel.kt
@@ -5,20 +5,25 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import org.positive.daymotion.common.SingleLiveEvent
-import org.positive.daymotion.presentation.base.subscriber.*
+import org.positive.daymotion.presentation.base.subscriber.CompletableSubscriber
+import org.positive.daymotion.presentation.base.subscriber.MaybeSubscriber
+import org.positive.daymotion.presentation.base.subscriber.SingleSubscriber
 
-abstract class BaseViewModel constructor(
-    disposables: CompositeDisposable = CompositeDisposable(),
-    isLoading: MutableLiveData<Boolean> = MutableLiveData<Boolean>(),
-) : ViewModel(),
-    SingleSubscriber by SingleSubscriberImpl(disposables, isLoading),
-    MaybeSubscriber by MaybeSubscriberImpl(disposables, isLoading),
-    CompletableSubscriber by CompletableSubscriberImpl(disposables, isLoading) {
+abstract class BaseViewModel : ViewModel(),
+    SingleSubscriber, MaybeSubscriber, CompletableSubscriber {
 
-    val isLoading: LiveData<Boolean> = isLoading
+    private val _disposables = CompositeDisposable()
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
 
     private val _showErrorMessageEvent: SingleLiveEvent<String> = SingleLiveEvent()
     val showErrorMessageEvent: LiveData<String> = _showErrorMessageEvent
+
+    override val disposables: CompositeDisposable
+        get() = _disposables
+    override val loadingMutableLiveData: MutableLiveData<Boolean>
+        get() = _isLoading
 
     protected fun showErrorMessage(message: String) {
         _showErrorMessageEvent.value = message
