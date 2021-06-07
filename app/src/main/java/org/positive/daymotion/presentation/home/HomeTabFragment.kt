2af397b1a8a5f4b599460286d8392f0a20c6b645
Yonghun Kim <sabgilhun@gmail.com>
2021-06-07 22:41:26 +0900

    add home tab base

---
+++ app/src/main/java/org/positive/daymotion/presentation/home/HomeTabFragment.kt
@@ -0,0 +1,9 @@
+package org.positive.daymotion.presentation.home
+
+import dagger.hilt.android.AndroidEntryPoint
+import org.positive.daymotion.R
+import org.positive.daymotion.databinding.FragmentHomeTabBinding
+import org.positive.daymotion.presentation.base.BaseFragment
+
+@AndroidEntryPoint
+class HomeTabFragment : BaseFragment<FragmentHomeTabBinding>(R.layout.fragment_home_tab)
