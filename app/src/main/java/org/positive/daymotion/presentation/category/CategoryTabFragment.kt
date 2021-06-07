bef89ea32c61363fd0cc67818b79d14eff187f30
Yonghun Kim <sabgilhun@gmail.com>
2021-06-07 22:46:10 +0900

    add category tab base

---
+++ app/src/main/java/org/positive/daymotion/presentation/category/CategoryTabFragment.kt
@@ -0,0 +1,9 @@
+package org.positive.daymotion.presentation.category
+
+import dagger.hilt.android.AndroidEntryPoint
+import org.positive.daymotion.R
+import org.positive.daymotion.databinding.FragmentCategoryTabBinding
+import org.positive.daymotion.presentation.base.BaseFragment
+
+@AndroidEntryPoint
+class CategoryTabFragment : BaseFragment<FragmentCategoryTabBinding>(R.layout.fragment_category_tab)
