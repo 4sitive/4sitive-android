eefd5aac35d18b2554a8d9dcfe16e71ede7f41fc
Yonghun Kim <sabgilhun@gmail.com>
2021-06-07 22:46:10 +0900

    add category tab base

---
+++ app/src/main/java/org/positive/daymotion/presentation/category/CategoryTabViewModel.kt
@@ -0,0 +1,8 @@
+package org.positive.daymotion.presentation.category
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.positive.daymotion.presentation.base.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CategoryTabViewModel @Inject constructor() : BaseViewModel()
