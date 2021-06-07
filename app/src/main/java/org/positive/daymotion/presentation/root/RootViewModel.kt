abf45529226cfd9b34fc9256412a107947b35f96
Yonghun Kim <sabgilhun@gmail.com>
2021-06-07 19:57:04 +0900

    add root activity base

--- app/src/main/java/org/positive/daymotion/presentation/root/RootViewModel.kt
+++ app/src/main/java/org/positive/daymotion/presentation/root/RootViewModel.kt
@@ -1,8 +1,8 @@
-package org.positive.daymotion.presentation.home
+package org.positive.daymotion.presentation.root
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.positive.daymotion.presentation.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : BaseViewModel()
+class RootViewModel @Inject constructor() : BaseViewModel()
