a03e1977515dc95be8d3c8a3b93009baccf52980
Yonghun Kim <sabgilhun@gmail.com>
2021-06-07 22:10:36 +0900

    Improve structure of subscriber classes

--- app/src/main/java/org/positive/daymotion/presentation/base/subscriber/MaybeSubscriber.kt
+++ app/src/main/java/org/positive/daymotion/presentation/base/subscriber/MaybeSubscriber.kt
@@ -1,40 +1,26 @@
 package org.positive.daymotion.presentation.base.subscriber
 
-import androidx.lifecycle.MutableLiveData
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Maybe
-import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-interface MaybeSubscriber {
+interface MaybeSubscriber : BaseSubscriber {
 
-    fun <T> Maybe<T>.autoDispose(block: MaybeSubscribeScope<T>.() -> Unit)
-
-    fun <T> Maybe<T>.backgroundCompose(): Maybe<T>
-
-    fun <T> Maybe<T>.apiLoadingCompose(): Maybe<T>
-}
-
-class MaybeSubscriberImpl(
-    private val disposables: CompositeDisposable,
-    private val _isLoading: MutableLiveData<Boolean>
-) : MaybeSubscriber {
-
-    override fun <T> Maybe<T>.autoDispose(block: MaybeSubscribeScope<T>.() -> Unit) {
+    fun <T> Maybe<T>.autoDispose(block: MaybeSubscribeScope<T>.() -> Unit) {
         val scope = MaybeSubscribeScope(this, disposables)
         scope.block()
         scope.subscribe()
     }
 
-    override fun <T> Maybe<T>.backgroundCompose(): Maybe<T> = compose {
+    fun <T> Maybe<T>.backgroundCompose(): Maybe<T> = compose {
         it.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
     }
 
-    override fun <T> Maybe<T>.apiLoadingCompose(): Maybe<T> = compose {
+    fun <T> Maybe<T>.apiLoadingCompose(): Maybe<T> = compose {
         it.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .doOnSubscribe { _isLoading.value = true }
-            .doFinally { _isLoading.value = false }
+            .doOnSubscribe { loadingMutableLiveData.value = true }
+            .doFinally { loadingMutableLiveData.value = false }
     }
 }
--- app/src/main/java/org/positive/daymotion/presentation/base/subscriber/SingleSubscriber.kt
+++ app/src/main/java/org/positive/daymotion/presentation/base/subscriber/SingleSubscriber.kt
@@ -1,40 +1,26 @@
 package org.positive.daymotion.presentation.base.subscriber
 
-import androidx.lifecycle.MutableLiveData
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-interface SingleSubscriber {
+interface SingleSubscriber : BaseSubscriber {
 
-    fun <T> Single<T>.autoDispose(block: SingleSubscribeScope<T>.() -> Unit)
-
-    fun <T> Single<T>.backgroundCompose(): Single<T>
-
-    fun <T> Single<T>.apiLoadingCompose(): Single<T>
-}
-
-class SingleSubscriberImpl(
-    private val disposables: CompositeDisposable,
-    private val _isLoading: MutableLiveData<Boolean>
-) : SingleSubscriber {
-
-    override fun <T> Single<T>.autoDispose(block: SingleSubscribeScope<T>.() -> Unit) {
+    fun <T> Single<T>.autoDispose(block: SingleSubscribeScope<T>.() -> Unit) {
         val scope = SingleSubscribeScope(this, disposables)
         scope.block()
         scope.subscribe()
     }
 
-    override fun <T> Single<T>.backgroundCompose(): Single<T> = compose {
+    fun <T> Single<T>.backgroundCompose(): Single<T> = compose {
         it.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
     }
 
-    override fun <T> Single<T>.apiLoadingCompose(): Single<T> = compose {
+    fun <T> Single<T>.apiLoadingCompose(): Single<T> = compose {
         it.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .doOnSubscribe { _isLoading.value = true }
-            .doFinally { _isLoading.value = false }
+            .doOnSubscribe { loadingMutableLiveData.value = true }
+            .doFinally { loadingMutableLiveData.value = false }
     }
 }
