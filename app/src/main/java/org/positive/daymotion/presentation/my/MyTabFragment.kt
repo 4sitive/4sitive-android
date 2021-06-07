076a448e47b2b1492ac0ed1cc3e56746ec5ed5cc
Yonghun Kim <sabgilhun@gmail.com>
2021-06-07 22:52:55 +0900

    add my tab base

---
+++ app/src/main/java/org/positive/daymotion/presentation/my/MyTabFragment.kt
@@ -0,0 +1,21 @@
+package org.positive.daymotion.presentation.my
+
+import android.os.Bundle
+import android.view.View
+import dagger.hilt.android.AndroidEntryPoint
+import org.positive.daymotion.R
+import org.positive.daymotion.databinding.FragmentCategoryTabBinding
+import org.positive.daymotion.databinding.FragmentMyTabBinding
+import org.positive.daymotion.presentation.base.BaseFragment
+import org.positive.daymotion.presentation.base.util.viewModelOf
+
+@AndroidEntryPoint
+class MyTabFragment : BaseFragment<FragmentMyTabBinding>(R.layout.fragment_my_tab) {
+
+    private val viewModel by viewModelOf<MyTabViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+    }
+}
