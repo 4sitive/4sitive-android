8688540eba6401919557d38edcc87f62ce37a6b1
Yonghun Kim <sabgilhun@gmail.com>
2021-06-07 22:55:42 +0900

    add setting tab base

---
+++ app/src/main/java/org/positive/daymotion/presentation/setting/SettingTabViewModel.kt
@@ -0,0 +1,8 @@
+package org.positive.daymotion.presentation.setting
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.positive.daymotion.presentation.base.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingTabViewModel @Inject constructor() : BaseViewModel()
