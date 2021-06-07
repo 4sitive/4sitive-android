f7c070e51d21a48b2ff41d8a30d4219897b7a2ad
Yonghun Kim <sabgilhun@gmail.com>
2021-06-07 22:49:57 +0900

    add missing view model link

--- app/src/main/java/org/positive/daymotion/presentation/home/HomeTabFragment.kt
+++ app/src/main/java/org/positive/daymotion/presentation/home/HomeTabFragment.kt
@@ -1,9 +1,20 @@
 package org.positive.daymotion.presentation.home
 
+import android.os.Bundle
+import android.view.View
 import dagger.hilt.android.AndroidEntryPoint
 import org.positive.daymotion.R
 import org.positive.daymotion.databinding.FragmentHomeTabBinding
 import org.positive.daymotion.presentation.base.BaseFragment
+import org.positive.daymotion.presentation.base.util.viewModelOf
 
 @AndroidEntryPoint
-class HomeTabFragment : BaseFragment<FragmentHomeTabBinding>(R.layout.fragment_home_tab)
+class HomeTabFragment : BaseFragment<FragmentHomeTabBinding>(R.layout.fragment_home_tab) {
+
+    private val viewModel by viewModelOf<HomeTabViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+    }
+}
