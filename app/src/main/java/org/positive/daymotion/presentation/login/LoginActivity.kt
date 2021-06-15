46d621076471462f7155faf5e34f751cccc2716b
kimjieun <mini1385@innotree.com>
2021-06-15 18:22:39 +0900

    용훈님 수정 요청 반영

--- app/src/main/java/org/positive/daymotion/presentation/login/LoginActivity.kt
+++ app/src/main/java/org/positive/daymotion/presentation/login/LoginActivity.kt
@@ -12,7 +12,6 @@ import android.text.util.Linkify
 import android.widget.Button
 import android.widget.TextView
 import androidx.browser.customtabs.CustomTabsIntent
-import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import dagger.hilt.android.AndroidEntryPoint
 import org.positive.daymotion.BuildConfig
@@ -31,84 +30,86 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
         super.onCreate(savedInstanceState)
         binding.handler = Handler()
 
-        window.statusBarColor = ContextCompat.getColor(applicationContext!!, R.color.colorWhite)
-
-        fun btnSet(btn: Button, str: String, word: String) {
-            val start: Int = str.indexOf(word)
-            val end: Int = start + word.length
-
-            val spannable = SpannableString(str)
-            spannable.setSpan(object : TypefaceSpan(null) {
-                override fun updateDrawState(ds: TextPaint) {
-                    ds.typeface = Typeface.create(ResourcesCompat.getFont(applicationContext, R.font.kopub_dotum_bold), Typeface.NORMAL)
-                }
-            }, start, end, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
-            btn.text = spannable
-        }
-
-        val googleBtn = findViewById<Button>(R.id.googleLoginButton)
+        val googleBtn = binding.googleLoginButton
         val googleStr = "Google"
         googleBtn.text = String.format(resources.getString(R.string.login_button), googleStr)
         btnSet(googleBtn, googleBtn.text.toString(), googleStr)
 
-        val naverBtn = findViewById<Button>(R.id.naverLoginButton)
+        val naverBtn = binding.naverLoginButton
         val naverStr = "Naver"
         naverBtn.text = String.format(resources.getString(R.string.login_button), naverStr)
         btnSet(naverBtn, naverBtn.text.toString(), naverStr)
 
-        val facebookBtn = findViewById<Button>(R.id.facebookLoginButton)
+        val facebookBtn = binding.facebookLoginButton
         val facebookStr = "Facebook"
         facebookBtn.text = String.format(resources.getString(R.string.login_button), facebookStr)
         btnSet(facebookBtn, facebookBtn.text.toString(), facebookStr)
 
-        val kakaoBtn = findViewById<Button>(R.id.kakaoLoginButton)
+        val kakaoBtn = binding.kakaoLoginButton
         val kakaoStr = "kakaoTalk"
         kakaoBtn.text = String.format(resources.getString(R.string.login_button), kakaoStr)
         btnSet(kakaoBtn, kakaoBtn.text.toString(), kakaoStr)
 
-        fun tvSet(tv: TextView, str: String, words: ArrayList<String>) {
-            val spannable = SpannableString(str)
-            for(item in words){
-                val start: Int = str.indexOf(item)
-                val end: Int = start + item.length
-
-                spannable.setSpan(object : TypefaceSpan(null) {
-                    override fun updateDrawState(ds: TextPaint) {
-                        ds.typeface = Typeface.create(ResourcesCompat.getFont(applicationContext, R.font.kopub_dotum_bold), Typeface.NORMAL)
-                    }
-                }, start, end, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
-            }
-            tv.text = spannable
-
-            val mTransform = Linkify.TransformFilter { match, url -> "" }
-
-            val serviceUrl = "https://www.naver.com/"
-            val privacyUrl = "https://www.daum.net/"
-            val urlArr = arrayListOf(serviceUrl, privacyUrl)
-
-            for((i, item) in words.withIndex()){
-                val pattern = Pattern.compile(item)
-                Linkify.addLinks(tv, pattern, urlArr[i], null, mTransform)
-            }
-        }
-
-        val tvInfo = findViewById<TextView>(R.id.tvInfo)
+        val tvInfo = binding.tvInfo
         val serviceStr = "서비스 약관"
         val privacyStr = "개인정보 보호정책"
         val strArr = arrayListOf(serviceStr, privacyStr)
         tvSet(tvInfo, tvInfo.text.toString(), strArr)
     }
 
+    private fun btnSet(btn: Button, str: String, word: String) {
+        val start: Int = str.indexOf(word)
+        val end: Int = start + word.length
+
+        val spannable = SpannableString(str)
+        spannable.setSpan(object : TypefaceSpan(null) {
+            override fun updateDrawState(ds: TextPaint) {
+                ds.typeface = Typeface.create(ResourcesCompat.getFont(applicationContext, R.font.kopub_dotum_bold), Typeface.NORMAL)
+            }
+        }, start, end, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+        btn.text = spannable
+
+//        btn.setOnClickListener(){
+//            btn.alpha = 0.5f
+//        }
+    }
+
+    private fun tvSet(tv: TextView, str: String, words: ArrayList<String>) {
+        val spannable = SpannableString(str)
+        for (item in words) {
+            val start: Int = str.indexOf(item)
+            val end: Int = start + item.length
+
+            spannable.setSpan(object : TypefaceSpan(null) {
+                override fun updateDrawState(ds: TextPaint) {
+                    ds.typeface = Typeface.create(ResourcesCompat.getFont(applicationContext, R.font.kopub_dotum_bold), Typeface.NORMAL)
+                }
+            }, start, end, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+        }
+        tv.text = spannable
+
+//        val mTransform = Linkify.TransformFilter { match, url -> "" }
+//
+//        val serviceUrl = "https://www.naver.com/"
+//        val privacyUrl = "https://www.daum.net/"
+//        val urlArr = arrayListOf(serviceUrl, privacyUrl)
+//
+//        for ((i, item) in words.withIndex()) {
+//            val pattern = Pattern.compile(item)
+//            Linkify.addLinks(tv, pattern, urlArr[i], null, mTransform)
+//        }
+    }
+
     inner class Handler {
         // TODO(yh): need loading bar
         fun requestLogin(loginWay: LoginWay) {
             val url = Uri.parse(DmConstants.ACCOUNT_SERVER_BASE_URL + "/oauth/authorize")
-                .buildUpon()
-                .appendQueryParameter("client_id", BuildConfig.OAUTH_CLIENT_ID)
-                .appendQueryParameter("redirect_uri", DmConstants.APP_SCHEME + "://login")
-                .appendQueryParameter("response_type", "code")
-                .appendQueryParameter("registration_hint", loginWay.registrationHint)
-                .build()
+                    .buildUpon()
+                    .appendQueryParameter("client_id", BuildConfig.OAUTH_CLIENT_ID)
+                    .appendQueryParameter("redirect_uri", DmConstants.APP_SCHEME + "://login")
+                    .appendQueryParameter("response_type", "code")
+                    .appendQueryParameter("registration_hint", loginWay.registrationHint)
+                    .build()
 
             val builder = CustomTabsIntent.Builder()
             val customTabsIntent = builder.build()
