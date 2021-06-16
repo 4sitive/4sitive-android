8bc273fe373164296a4e33484d9b6e6498778913
Yonghun Kim <sabgilhun@gmail.com>
2021-06-17 02:55:44 +0900

    implement empty mission history ui

--- app/src/main/java/org/positive/daymotion/presentation/category/model/MissionHistoryItem.kt
+++ app/src/main/java/org/positive/daymotion/presentation/category/model/MissionHistoryItem.kt
@@ -2,16 +2,14 @@ package org.positive.daymotion.presentation.category.model
 
 data class MissionHistoryItem(
     val date: String,
-    val missions: List<MissionHistoryInnerItem>,
-    val savedPosition: Int = 0,
-    val savedPositionOffset: Int = 0
+    val missions: List<MissionHistoryInnerItem>
 ) {
 
     val isMissionEmpty = missions.isEmpty()
 
     val firstMission = missions.getOrNull(0)
 
-    val secondMission = missions.getOrNull(0)
+    val secondMission = missions.getOrNull(1)
 
-    val thirdMission = missions.getOrNull(0)
+    val thirdMission = missions.getOrNull(2)
 }
