20ad6d6921381666b59d87a9530cb6b19a2aef43
Yonghun Kim <sabgilhun@gmail.com>
2021-06-17 02:17:42 +0900

    modify category browser page ui

--- app/src/main/java/org/positive/daymotion/presentation/category/adapter/CategoryBrowserAdapter.kt
+++ app/src/main/java/org/positive/daymotion/presentation/category/adapter/CategoryBrowserAdapter.kt
@@ -2,8 +2,7 @@ package org.positive.daymotion.presentation.category.adapter
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import org.positive.daymotion.databinding.ItemCategoryBrowserLargeBinding
-import org.positive.daymotion.databinding.ItemCategoryBrowserSmallBinding
+import org.positive.daymotion.databinding.ItemCategoryBrowserBinding
 import org.positive.daymotion.extension.layoutInflater
 import org.positive.daymotion.presentation.category.adapter.holder.CategoryBrowserViewHolder
 import org.positive.daymotion.presentation.category.model.CategoryBrowserItem
@@ -25,17 +24,8 @@ class CategoryBrowserAdapter : RecyclerView.Adapter<CategoryBrowserViewHolder>()
         viewType: Int
     ): CategoryBrowserViewHolder {
         val layoutInflater = parent.context.layoutInflater
-        return when (viewType) {
-            0 -> {
-                val binding = ItemCategoryBrowserSmallBinding.inflate(layoutInflater, parent, false)
-                CategoryBrowserViewHolder.SmallType(binding)
-            }
-            1 -> {
-                val binding = ItemCategoryBrowserLargeBinding.inflate(layoutInflater, parent, false)
-                CategoryBrowserViewHolder.LargeType(binding)
-            }
-            else -> throw IllegalArgumentException("Invalid view type")
-        }
+        val binding = ItemCategoryBrowserBinding.inflate(layoutInflater, parent, false)
+        return CategoryBrowserViewHolder(binding)
     }
 
     override fun onBindViewHolder(
@@ -43,13 +33,8 @@ class CategoryBrowserAdapter : RecyclerView.Adapter<CategoryBrowserViewHolder>()
         position: Int
     ) {
         val item = items[position]
-        when (holder) {
-            is CategoryBrowserViewHolder.SmallType -> holder.binding.item = item
-            is CategoryBrowserViewHolder.LargeType -> holder.binding.item = item
-        }
+        holder.binding.item = item
     }
 
     override fun getItemCount() = items.size
-
-    override fun getItemViewType(position: Int) = if (items[position].isSmallType) 0 else 1
 }
--- app/src/main/java/org/positive/daymotion/presentation/category/model/CategoryBrowserItem.kt
+++ app/src/main/java/org/positive/daymotion/presentation/category/model/CategoryBrowserItem.kt
@@ -1,10 +1,48 @@
 package org.positive.daymotion.presentation.category.model
 
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import org.positive.daymotion.R
+import java.text.DecimalFormat
+import kotlin.random.Random
+
 data class CategoryBrowserItem(
     val categoryName: String,
     val participants: Int
 ) {
-    val isSmallType = participants < 100
 
-    val formattedParticipants = "#$participants"
+    @get:DrawableRes
+    val background = when {
+        participants > 100 -> {
+            when (RANDOM_SEED++.rem(3)) {
+                0 -> R.drawable.img_category_01
+                1 -> R.drawable.img_category_02
+                else -> R.drawable.img_category_03
+            }
+        }
+        participants in 11..100 -> R.drawable.category_tab_item_background
+        else -> R.color._F1F1F1
+    }
+
+    val isVisibleParticipants = participants > 100
+
+    @get:ColorRes
+    val nameTextColorRes = if (participants > 100) {
+        R.color._FFFFFF
+    } else {
+        R.color._000000
+    }
+
+    val nameTextAlpha = if (participants > 100) {
+        1.0f
+    } else {
+        0.6f
+    }
+
+    val formattedParticipants = "# ${decimalFormatter.format(participants)}"
+
+    companion object {
+        private var RANDOM_SEED = Random.nextInt(0, 2)
+        private val decimalFormatter = DecimalFormat("###,###")
+    }
 }
