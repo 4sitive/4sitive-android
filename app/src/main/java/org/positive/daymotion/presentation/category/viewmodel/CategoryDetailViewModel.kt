ebfdd8a6a1222c255d9c6bd2837feaca84185fac
Yonghun Kim <sabgilhun@gmail.com>
2021-06-17 17:34:02 +0900

    implement category detail empty view

--- app/src/main/java/org/positive/daymotion/presentation/category/viewmodel/CategoryDetailViewModel.kt
+++ app/src/main/java/org/positive/daymotion/presentation/category/viewmodel/CategoryDetailViewModel.kt
@@ -2,10 +2,12 @@ package org.positive.daymotion.presentation.category.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.positive.daymotion.presentation.base.BaseViewModel
 import org.positive.daymotion.presentation.category.model.CategoryDetailItem
 import javax.inject.Inject
+import kotlin.random.Random
 
 @HiltViewModel
 class CategoryDetailViewModel @Inject constructor() : BaseViewModel() {
@@ -13,12 +15,19 @@ class CategoryDetailViewModel @Inject constructor() : BaseViewModel() {
     private val _categoryDetails = MutableLiveData<List<CategoryDetailItem>>()
     val categoryDetails: LiveData<List<CategoryDetailItem>> get() = _categoryDetails
 
+    val isEmptyDetailItem: LiveData<Boolean> =
+        Transformations.map(_categoryDetails) { items -> items.isEmpty() }
+
     fun loadCategoryDetails() {
-        _categoryDetails.value = buildList {
-            add(CategoryDetailItem("", CategoryDetailItem.ImageType.PORTRAIT))
-            add(CategoryDetailItem("", CategoryDetailItem.ImageType.LANDSCAPE))
-            add(CategoryDetailItem("", CategoryDetailItem.ImageType.PORTRAIT))
-            add(CategoryDetailItem("", CategoryDetailItem.ImageType.LANDSCAPE))
+        if (Random.nextInt().rem(5) == 0) {
+            _categoryDetails.value = emptyList()
+        } else {
+            _categoryDetails.value = buildList {
+                add(CategoryDetailItem("", CategoryDetailItem.ImageType.PORTRAIT))
+                add(CategoryDetailItem("", CategoryDetailItem.ImageType.LANDSCAPE))
+                add(CategoryDetailItem("", CategoryDetailItem.ImageType.PORTRAIT))
+                add(CategoryDetailItem("", CategoryDetailItem.ImageType.LANDSCAPE))
+            }
         }
     }
 }
