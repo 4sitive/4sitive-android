a6bc0e0bbeb152fabcde16ad18a4f2853e2f636c
Yonghun Kim <sabgilhun@gmail.com>
2021-06-24 21:12:26 +0900

    add logic for nickname validation

---
+++ app/src/main/java/org/positive/daymotion/presentation/my/model/NickNameValidation.kt
@@ -0,0 +1,5 @@
+package org.positive.daymotion.presentation.my.model
+
+enum class NickNameValidation {
+    OK, DENIED_SPECIAL_CHAR, DENIED_TOO_LONG
+}
--- app/src/main/java/org/positive/daymotion/presentation/my/viewmodel/MyProfileEditViewModel.kt
+++ app/src/main/java/org/positive/daymotion/presentation/my/viewmodel/MyProfileEditViewModel.kt
@@ -2,16 +2,21 @@ package org.positive.daymotion.presentation.my.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.positive.daymotion.common.SingleLiveEvent
 import org.positive.daymotion.presentation.base.BaseViewModel
+import org.positive.daymotion.presentation.my.model.NickNameValidation
 import javax.inject.Inject
 
 @HiltViewModel
 class MyProfileEditViewModel @Inject constructor() : BaseViewModel() {
 
     val name = MutableLiveData<String>()
+
     val introduce = MutableLiveData<String>()
+
+    val nickNameValidation = Transformations.map(name) { checkNickNameValidation(it) }
 
     private val _doneProfileUpdate = SingleLiveEvent<Nothing>()
     val doneProfileUpdate: LiveData<Nothing> get() = _doneProfileUpdate
@@ -23,5 +28,15 @@ class MyProfileEditViewModel @Inject constructor() : BaseViewModel() {
 
     fun updateProfile() {
         _doneProfileUpdate.call()
+    }
+
+    private fun checkNickNameValidation(input: String) = when {
+        nickNameValidateRegex.matchEntire(input) == null -> NickNameValidation.DENIED_SPECIAL_CHAR
+        input.length > 8 -> NickNameValidation.DENIED_TOO_LONG
+        else -> NickNameValidation.OK
+    }
+
+    companion object {
+        private val nickNameValidateRegex = Regex("^[a-zA-Z0-9가-힉 ]*$")
     }
 }
