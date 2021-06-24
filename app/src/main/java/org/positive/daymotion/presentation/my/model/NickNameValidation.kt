ff2dada906d3a137dad6f4a226c67a42a16272e7
Yonghun Kim <sabgilhun@gmail.com>
2021-06-24 21:25:02 +0900

    bind nickname validation value

--- app/src/main/java/org/positive/daymotion/presentation/my/model/NickNameValidation.kt
+++ app/src/main/java/org/positive/daymotion/presentation/my/model/NickNameValidation.kt
@@ -1,5 +1,7 @@
 package org.positive.daymotion.presentation.my.model
 
-enum class NickNameValidation {
-    OK, DENIED_SPECIAL_CHAR, DENIED_TOO_LONG
+enum class NickNameValidation(val reason: String) {
+    OK(""),
+    DENIED_SPECIAL_CHAR("유저 아이디에는 특수문자 사용이 불가합니다."),
+    DENIED_TOO_LONG("유저 아이디는 8글자 이상을 초과 할 수 없습니다.")
 }
