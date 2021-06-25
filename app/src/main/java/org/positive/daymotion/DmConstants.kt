9ba8af277ecb1ece072684fbe98f0e4bdb5ab35b
Yonghun Kim <sabgilhun@gmail.com>
2021-06-26 02:50:54 +0900

    delete todo

--- app/src/main/java/org/positive/daymotion/DmConstants.kt
+++ app/src/main/java/org/positive/daymotion/DmConstants.kt
@@ -1,11 +1,9 @@
 package org.positive.daymotion
 
 object DmConstants {
-    // TODO: depend on variant
     const val ACCOUNT_SERVER_BASE_URL = "https://account.4sitive.com/"
 
     const val CDN_SERVER_BASE_URL = "https://cdn.4sitive.com/"
 
-    // TODO: depend on variant
     const val APP_SCHEME = "positive"
 }
