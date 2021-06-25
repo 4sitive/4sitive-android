de86d3ae4630a3940510644baf7e99f4dbf90070
Yonghun Kim <sabgilhun@gmail.com>
2021-06-26 00:36:34 +0900

    delete useless class

--- app/src/main/java/org/positive/daymotion/data/model/ServerTimeResponse.kt
+++
@@ -1,7 +0,0 @@
-package org.positive.daymotion.data.model
-
-import com.google.gson.annotations.SerializedName
-
-data class ServerTimeResponse(
-    @SerializedName("unixtime") val unixTime: Long
-)
