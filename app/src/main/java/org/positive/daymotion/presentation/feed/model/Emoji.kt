dc84bb2a98e57767f5c066928a1287c13ca026cd
Yonghun Kim <sabgilhun@gmail.com>
2021-06-27 21:33:20 +0900

    bind feed information

---
+++ app/src/main/java/org/positive/daymotion/presentation/feed/model/Emoji.kt
@@ -0,0 +1,6 @@
+package org.positive.daymotion.presentation.feed.model
+
+data class Emoji(
+    val emojiId: String,
+    val count: Int
+)
---
+++ app/src/main/java/org/positive/daymotion/presentation/feed/model/FeedInformation.kt
@@ -0,0 +1,20 @@
+package org.positive.daymotion.presentation.feed.model
+
+import android.graphics.Color
+import androidx.annotation.ColorInt
+import org.positive.daymotion.R
+
+data class FeedInformation(
+    val imageUrl: String,
+    val isLandscapeImage: Boolean,
+    val author: String,
+    val emojis: List<Emoji>,
+    val keyColor: String
+) {
+    @get:ColorInt
+    val keyColorResource = try {
+        Color.parseColor(keyColor)
+    } catch (e: IllegalArgumentException) {
+        R.color._000000
+    }
+}
