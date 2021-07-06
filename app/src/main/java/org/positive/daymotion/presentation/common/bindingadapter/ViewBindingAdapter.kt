f5441e86b9ddf44064ee5f965e02b5ac45402fe7
Yonghun Kim <sabgilhun@gmail.com>
2021-07-07 01:27:10 +0900

    add binding adapter for setting press effect

--- app/src/main/java/org/positive/daymotion/presentation/common/bindingadapter/ViewBindingAdapter.kt
+++ app/src/main/java/org/positive/daymotion/presentation/common/bindingadapter/ViewBindingAdapter.kt
@@ -1,10 +1,14 @@
 package org.positive.daymotion.presentation.common.bindingadapter
 
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.StateListDrawable
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
+import org.positive.daymotion.R
 
 
 @BindingAdapter("isVisible")
@@ -24,4 +28,15 @@ fun View.setConstraintDimensionRatioBindingAdapter(ratio: String) {
         layoutParams.dimensionRatio = ratio
         setLayoutParams(layoutParams)
     }
+}
+
+@BindingAdapter("pressEffect")
+fun View.setPressEffect(withGaryBg: Boolean) {
+    val stateListDrawable = StateListDrawable().apply {
+        setExitFadeDuration(300)
+        val pressedColor = if (withGaryBg) R.color._80000000 else R.color._80FFFFFF
+        val colorDrawable = ColorDrawable(ContextCompat.getColor(context, pressedColor))
+        addState(intArrayOf(android.R.attr.state_pressed), colorDrawable)
+    }
+    foreground = stateListDrawable
 }
