ab886600ecf7705c78dab8585a348cf8949915cf
Yonghun Kim <sabgilhun@gmail.com>
2021-07-12 02:57:43 +0900

    link oauth

--- app/src/main/java/org/positive/daymotion/presentation/login/AgreementActivity.kt
+++ app/src/main/java/org/positive/daymotion/presentation/login/AgreementActivity.kt
@@ -1,8 +1,12 @@
 package org.positive.daymotion.presentation.login
 
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import androidx.browser.customtabs.CustomTabsIntent
 import dagger.hilt.android.AndroidEntryPoint
+import org.positive.daymotion.BuildConfig
+import org.positive.daymotion.DmConstants
 import org.positive.daymotion.R
 import org.positive.daymotion.databinding.ActivityAgreementBinding
 import org.positive.daymotion.presentation.common.base.BaseActivity
@@ -16,11 +20,31 @@ import org.positive.daymotion.presentation.login.viewmodel.AgreementViewModel
 class AgreementActivity : BaseActivity<ActivityAgreementBinding>(R.layout.activity_agreement) {
 
     private val loginWay by bundle<LoginWay>()
+
+    private val handler = Handler()
     private val viewModel by viewModelOf<AgreementViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.handler = handler
         binding.viewModel = viewModel
+    }
+
+    inner class Handler {
+        fun requestOauthLogin() {
+            CustomTabsIntent.Builder()
+                .build()
+                .launchUrl(this@AgreementActivity, buildOauthRequestUrl())
+        }
+
+        private fun buildOauthRequestUrl() =
+            Uri.parse(DmConstants.ACCOUNT_SERVER_BASE_URL + "/oauth/authorize")
+                .buildUpon()
+                .appendQueryParameter("client_id", BuildConfig.OAUTH_CLIENT_ID)
+                .appendQueryParameter("redirect_uri", DmConstants.APP_SCHEME + "://login")
+                .appendQueryParameter("response_type", "code")
+                .appendQueryParameter("registration_hint", loginWay.registrationHint)
+                .build()
     }
 
     companion object {
