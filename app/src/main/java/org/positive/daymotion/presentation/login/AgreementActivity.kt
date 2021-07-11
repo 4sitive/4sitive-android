8a9bc5d965761ae9ec7cd67d7922638172b399cc
Yonghun Kim <sabgilhun@gmail.com>
2021-07-12 01:54:02 +0900

    implement agreement activity layout

---
+++ app/src/main/java/org/positive/daymotion/presentation/login/AgreementActivity.kt
@@ -0,0 +1,10 @@
+package org.positive.daymotion.presentation.login
+
+import dagger.hilt.android.AndroidEntryPoint
+import org.positive.daymotion.R
+import org.positive.daymotion.databinding.ActivityAgreementBinding
+import org.positive.daymotion.presentation.common.base.BaseActivity
+
+@AndroidEntryPoint
+class AgreementActivity : BaseActivity<ActivityAgreementBinding>(R.layout.activity_agreement) {
+}
