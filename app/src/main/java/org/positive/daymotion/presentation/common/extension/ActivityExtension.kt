2669b4499c8f396f67abef2115ba701ff79604cc
Yonghun Kim <sabgilhun@gmail.com>
2021-07-18 16:14:16 +0900

    clean code FeeUploadActivity with FragmentChangeManager

--- app/src/main/java/org/positive/daymotion/presentation/common/extension/ActivityExtension.kt
+++ app/src/main/java/org/positive/daymotion/presentation/common/extension/ActivityExtension.kt
@@ -3,6 +3,13 @@ package org.positive.daymotion.presentation.common.extension
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 
 fun ComponentActivity.registerActivityResult(handleResult: (ActivityResult) -> Unit) =
     registerForActivityResult(ActivityResultContracts.StartActivityForResult(), handleResult)
+
+inline fun <reified T : Fragment> FragmentActivity.findFragment(): T? =
+    supportFragmentManager.fragments
+        .filterIsInstance(T::class.java)
+        .firstOrNull()
