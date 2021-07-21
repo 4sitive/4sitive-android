61819c9fa02a7dab2148393770e72f00f619583b
Yonghun Kim <sabgilhun@gmail.com>
2021-07-22 02:05:04 +0900

    add alignment select ui

--- app/src/main/java/org/positive/daymotion/presentation/upload/viewmodel/UploadFeedTextEditViewModel.kt
+++ app/src/main/java/org/positive/daymotion/presentation/upload/viewmodel/UploadFeedTextEditViewModel.kt
@@ -1,5 +1,6 @@
 package org.positive.daymotion.presentation.upload.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.positive.daymotion.presentation.common.base.BaseViewModel
 import javax.inject.Inject
@@ -7,4 +8,7 @@ import javax.inject.Inject
 @HiltViewModel
 class UploadFeedTextEditViewModel @Inject constructor() : BaseViewModel() {
 
+    val isSelectedAlignmentLeft = MutableLiveData<Boolean>()
+    val isSelectedAlignmentCenter = MutableLiveData<Boolean>()
+    val isSelectedAlignmentRight = MutableLiveData<Boolean>()
 }
