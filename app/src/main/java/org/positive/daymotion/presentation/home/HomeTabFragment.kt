536652155d10c5175856fe688d39b3aa1fd81820
Yonghun Kim <sabgilhun@gmail.com>
2021-07-31 03:16:08 +0900

    refactoring home tab and post list

--- app/src/main/java/org/positive/daymotion/presentation/home/HomeTabFragment.kt
+++ app/src/main/java/org/positive/daymotion/presentation/home/HomeTabFragment.kt
@@ -1,73 +1,67 @@
 package org.positive.daymotion.presentation.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.core.content.ContextCompat
+import com.google.android.material.appbar.AppBarLayout
 import dagger.hilt.android.AndroidEntryPoint
 import org.positive.daymotion.R
 import org.positive.daymotion.databinding.FragmentHomeTabBinding
 import org.positive.daymotion.presentation.common.ScrollableFragment
 import org.positive.daymotion.presentation.common.base.BaseFragment
 import org.positive.daymotion.presentation.common.base.viewModelOf
+import org.positive.daymotion.presentation.home.model.MissionViewItem
 
 
 @AndroidEntryPoint
-class HomeTabFragment : BaseFragment<FragmentHomeTabBinding>(R.layout.fragment_home_tab), ScrollableFragment {
+class HomeTabFragment : BaseFragment<FragmentHomeTabBinding>(R.layout.fragment_home_tab),
+    ScrollableFragment {
 
     private val viewModel by viewModelOf<HomeTabViewModel>()
-    private val missionArray = arrayListOf("아아? 라떼? 커피를 추천해줘!", "털복숭이 친구들을 소개해줘요!", "도시의 밤 향기를 닮은 너!")
+
+    private val missions = listOf(
+        "아아? 라떼? 커피를 추천해줘!",
+        "털복숭이 친구들을 소개해줘요!",
+        "도시의 밤 향기를 닮은 너!"
+    )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.missionItems = makeMissionViewItem(missions)
+        binding.handler = Handler()
         binding.viewModel = viewModel
+    }
 
-        val (randomColors, randomEffects) = selectBackground()
-        val cardArray = arrayListOf(binding.homeMissionCard1, binding.homeMissionCard2, binding.homeMissionCard3)
-        val effectArray = arrayListOf(binding.CardEffectImageView1, binding.CardEffectImageView2, binding.CardEffectImageView3)
-        val textViewArray = arrayListOf(binding.missionTextView1, binding.missionTextView2, binding.missionTextView3)
+    private fun makeMissionViewItem(missions: List<String>): List<MissionViewItem> {
+        val randomBackgroundList = mutableListOf(
+            R.drawable.bananamania to R.drawable.bananamania_art,
+            R.drawable.can to R.drawable.can_art,
+            R.drawable.chetwodeblue to R.drawable.chetwodeblue_art,
+            R.drawable.darksalmon to R.drawable.darksalmon_art,
+            R.drawable.flare to R.drawable.flare_art,
+            R.drawable.flax to R.drawable.flax_art,
+            R.drawable.fuchsia to R.drawable.fuchsia_art,
+            R.drawable.mediumpurple to R.drawable.mediumpurple_art,
+            R.drawable.mindaro to R.drawable.mindaro_art,
+            R.drawable.palevioletred to R.drawable.palevioletred_art,
+            R.drawable.ronchi to R.drawable.ronchi_art,
+            R.drawable.witch_haze to R.drawable.witch_haze_art,
+        ).apply { shuffle() }
 
-        for ((i, card) in cardArray.withIndex()) {
-            textViewArray[i].text = missionArray[i]
-            effectArray[i].setImageResource(randomEffects[i] as Int)
-            card.background = ContextCompat.getDrawable(requireContext(), randomColors[i] as Int)
-            card.setOnClickListener {
-                sendSubject(missionArray[i], randomColors[i] as Int, randomEffects[i] as Int)
-            }
+        return missions.mapIndexed { i, mission ->
+            MissionViewItem(
+                mission,
+                randomBackgroundList[i].first,
+                randomBackgroundList[i].second
+            )
         }
-    }
-
-    private fun sendSubject(mission: String, color: Int, effect: Int) {
-        val intent = Intent(requireContext(), PostListActivity::class.java)
-        intent.putExtra("mission", mission)
-        intent.putExtra("color", color)
-        intent.putExtra("effect", effect)
-        startActivity(intent)
-    }
-
-    private fun selectBackground(): Pair<ArrayList<Int?>, ArrayList<Int?>> {
-        val backgroundColorArray = arrayListOf(
-            mapOf("color" to R.drawable.bananamania, "effect" to R.drawable.bananamania_art),
-            mapOf("color" to R.drawable.can, "effect" to R.drawable.can_art),
-            mapOf("color" to R.drawable.chetwodeblue, "effect" to R.drawable.chetwodeblue_art),
-            mapOf("color" to R.drawable.darksalmon, "effect" to R.drawable.darksalmon_art),
-            mapOf("color" to R.drawable.flare, "effect" to R.drawable.flare_art),
-            mapOf("color" to R.drawable.flax, "effect" to R.drawable.flax_art),
-            mapOf("color" to R.drawable.fuchsia, "effect" to R.drawable.fuchsia_art),
-            mapOf("color" to R.drawable.mediumpurple, "effect" to R.drawable.mediumpurple_art),
-            mapOf("color" to R.drawable.mindaro, "effect" to R.drawable.mindaro_art),
-            mapOf("color" to R.drawable.palevioletred, "effect" to R.drawable.palevioletred_art),
-            mapOf("color" to R.drawable.ronchi, "effect" to R.drawable.ronchi_art),
-            mapOf("color" to R.drawable.witch_haze, "effect" to R.drawable.witch_haze_art),
-        )
-        val randomArray = backgroundColorArray.shuffled()
-        val colors = arrayListOf(randomArray[0]["color"], randomArray[1]["color"], randomArray[2]["color"])
-        val effects = arrayListOf(randomArray[0]["effect"], randomArray[1]["effect"], randomArray[2]["effect"])
-
-        return Pair(colors, effects)
     }
 
     override fun scrollToTop() {
         binding.scrollView.smoothScrollTo(0, binding.profileLayout.top)
+    }
+
+    inner class Handler {
+        fun sendSubject(missionViewItem: MissionViewItem) =
+            PostListActivity.start(requireContext(), missionViewItem)
     }
 }
--- app/src/main/java/org/positive/daymotion/presentation/home/PostListActivity.kt
+++ app/src/main/java/org/positive/daymotion/presentation/home/PostListActivity.kt
@@ -1,13 +1,16 @@
 package org.positive.daymotion.presentation.home
 
-import android.content.Intent
+import android.content.Context
 import android.os.Bundle
-import androidx.core.content.ContextCompat
+import com.google.android.material.appbar.AppBarLayout
 import dagger.hilt.android.AndroidEntryPoint
 import org.positive.daymotion.R
 import org.positive.daymotion.databinding.ActivityPostListBinding
 import org.positive.daymotion.presentation.common.base.BaseActivity
 import org.positive.daymotion.presentation.common.base.viewModelOf
+import org.positive.daymotion.presentation.common.bundle
+import org.positive.daymotion.presentation.common.extension.startWith
+import org.positive.daymotion.presentation.home.model.MissionViewItem
 import org.positive.daymotion.presentation.upload.activity.FeedUploadActivity
 
 @AndroidEntryPoint
@@ -15,46 +18,38 @@ class PostListActivity :
     BaseActivity<ActivityPostListBinding>(R.layout.activity_post_list) {
 
     private val viewModel by viewModelOf<PostListViewModel>()
+    private val missionViewItem by bundle<MissionViewItem>()
     private val handler by lazy { Handler() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
+        binding.missionViewItem = missionViewItem
         binding.handler = handler
+        
+        setupViews()
+    }
 
-        val mission = intent.getStringExtra("mission")
-
-        binding.whiteBackButton.setOnClickListener {
-            finish()
-        }
-        binding.blackBackButton.setOnClickListener {
-            finish()
-        }
-
-        binding.goToUploadButton.setOnClickListener {
-            val intent = Intent(this, FeedUploadActivity::class.java)
-            intent.putExtra("mission", mission)
-            startActivity(intent)
-        }
-        binding.minUploadButton.setOnClickListener {
-            val intent = Intent(this, FeedUploadActivity::class.java)
-            intent.putExtra("mission", mission)
-            startActivity(intent)
-        }
-
-        binding.missionTextView.text = mission
-        binding.titleTextView.text = mission
-
-        binding.homeMissionCard.background =
-            ContextCompat.getDrawable(this, intent.getIntExtra("color", 0))
-        binding.CardEffectImageView.setImageResource(intent.getIntExtra("effect", 0))
-
-
+    private fun setupViews() {
+        binding.appBarLayout.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, offset ->
+            val total = appBarLayout.totalScrollRange
+            val percentage = (total + offset) / total.toFloat()
+            binding.imageViewContainer.alpha = percentage
+            binding.homeMissionCard.alpha = percentage
+            binding.toolbar.alpha = 1 - percentage
+        })
     }
 
     inner class Handler {
-
+        fun finish() = this@PostListActivity.finish()
+        fun startFeedUploadActivity() = FeedUploadActivity.start(this@PostListActivity)
     }
 
+    companion object {
+        fun start(
+            context: Context,
+            missionViewItem: MissionViewItem
+        ) = context.startWith<PostListActivity>("missionViewItem" to missionViewItem)
+    }
 }
 
