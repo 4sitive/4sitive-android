eee4c6b02a8f258a2ce38fa1c656f873d413e005
Yonghun Kim <sabgilhun@gmail.com>
2021-07-31 16:58:43 +0900

    repackaging classes

--- app/src/main/java/org/positive/daymotion/presentation/home/fragment/HomeTabFragment.kt
+++ app/src/main/java/org/positive/daymotion/presentation/home/fragment/HomeTabFragment.kt
@@ -1,15 +1,16 @@
-package org.positive.daymotion.presentation.home
+package org.positive.daymotion.presentation.home.fragment
 
 import android.os.Bundle
 import android.view.View
-import com.google.android.material.appbar.AppBarLayout
 import dagger.hilt.android.AndroidEntryPoint
 import org.positive.daymotion.R
 import org.positive.daymotion.databinding.FragmentHomeTabBinding
 import org.positive.daymotion.presentation.common.ScrollableFragment
 import org.positive.daymotion.presentation.common.base.BaseFragment
 import org.positive.daymotion.presentation.common.base.viewModelOf
+import org.positive.daymotion.presentation.home.activity.PostListActivity
 import org.positive.daymotion.presentation.home.model.MissionViewItem
+import org.positive.daymotion.presentation.home.viewmodel.HomeTabViewModel
 
 
 @AndroidEntryPoint
