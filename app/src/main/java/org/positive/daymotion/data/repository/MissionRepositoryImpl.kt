c950d012919498ab2dac5bf8a9c0a16be81eefb4
Yonghun Kim <sabgilhun@gmail.com>
2021-09-11 16:55:03 +0900

    add logic checking yesterday

--- app/src/main/java/org/positive/daymotion/data/repository/MissionRepositoryImpl.kt
+++ app/src/main/java/org/positive/daymotion/data/repository/MissionRepositoryImpl.kt
@@ -4,6 +4,8 @@ import io.reactivex.rxjava3.core.Single
 import org.positive.daymotion.data.api.MissionApi
 import org.positive.daymotion.domain.DayMission
 import org.positive.daymotion.domain.Mission
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 class MissionRepositoryImpl @Inject constructor(
@@ -27,9 +29,15 @@ class MissionRepositoryImpl @Inject constructor(
 
     override fun loadLastMissions(): Single<List<DayMission>> {
         return missionApi.getLastMissions().map { response ->
+            val format = SimpleDateFormat("yyy-MM-dd")
+            val calendar = Calendar.getInstance().apply { time = format.parse(response.now) }
+            calendar.add(Calendar.DATE, -1)
+            val yesterday = format.format(calendar.time)
+
             val grouping = response.content.groupBy { it.date }
             grouping.map {
                 DayMission(
+                    yesterday == it.key,
                     it.key,
                     it.value.map { v ->
                         Mission(
